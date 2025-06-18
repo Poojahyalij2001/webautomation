57e8945565677d4e934b96990efa6a987533e1b6
pooja1601tech <69473066+pooja1601tech@users.noreply.github.com>
2025-06-18 08:00:20 +0530

    new repo

---
+++ src/main/java/webautomation.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class webautomation {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("www.github.com");
+		System.out.println("Hello");
+
+	}
+
+}
